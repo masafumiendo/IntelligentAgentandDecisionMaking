63afdf25d7c0f99600b96812a2bbc186cb92cbea
Masafumi Endo <endoma@oregonstate.edu>
2020-04-26 14:00:47 -0700

    add comparison

--- MDP/main_VI.py
+++ MDP/main_VI.py
@@ -17,7 +17,7 @@ run_time = {}
 
 def main():
 
-    MAP = map_50
+    MAP = map_32
     map_size = MAP[1]
 
     beta = 0.999
@@ -29,16 +29,13 @@ def main():
     v, pi = sync_value_iteration(env, beta=beta)
     v_np, pi_np = np.array(v), np.array(pi)
     end_time = time.time()
-    run_time['Sync Value Iteration v2'] = end_time - start_time
-    print("time:", run_time['Sync Value Iteration v2'])
+    run_time['Sync Value Iteration'] = end_time - start_time
+    print("time:", run_time['Sync Value Iteration'])
 
     print_results(v, pi, map_size, env, beta, 'sync_vi_gs')
 
     ray.shutdown()
     ray.init(include_webui=False, ignore_reinit_error=True, redis_max_memory=500000000, object_store_memory=5000000000)
-
-    MAP = map_50
-    map_size = MAP[1]
 
     beta = 0.999
     env = FrozenLakeEnv(desc=MAP[0], is_slippery=True)
@@ -49,9 +46,24 @@ def main():
     v, pi = distribured_value_iteraion(env, beta=beta, workers_num=8)
     v_np, pi_np = np.array(v), np.array(pi)
     end_time = time.time()
-    run_time['Sync distributed comp'] = end_time - start_time
-    print("time:", run_time['Sync distributed comp'])
-    print_results(v, pi, map_size, env, beta, 'dist_vi_comp')
+    run_time['Sync distributed VI'] = end_time - start_time
+    print("time:", run_time['Sync distributed VI'])
+    print_results(v, pi, map_size, env, beta, 'dist_vi')
+
+    from copy import deepcopy
+    temp_dict = deepcopy(run_time)
+    print("All:")
+    for _ in range(len(temp_dict)):
+        min_v = float('inf')
+        for k, v in temp_dict.items():
+            if v is None:
+                continue
+            if v < min_v:
+                min_v = v
+                name = k
+        temp_dict[name] = float('inf')
+        print(name + ": " + str(min_v))
+        print()
 
 if __name__ == '__main__':
     main()
